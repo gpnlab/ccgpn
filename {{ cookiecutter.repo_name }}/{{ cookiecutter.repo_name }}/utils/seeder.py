291910fe288b63f8ed6a8065e3fdcf464889d76c
eduardojdiniz <edd32@pitt.edu>
2021-09-01 07:25:49 -0400

    Update utils

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/utils/seeder.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/utils/seeder.py
@@ -7,7 +7,8 @@ import numpy as np
 import random
 import torch
 
-def set_seed(seed=None, seed_torch=True, seed_cudnn=False):
+
+def set_seed(logger, seed=None, seed_torch=True, seed_cudnn=False):
     """Set seed of random number generators to limit the number of sources of
     nondeterministic behavior for a specific platform, device, and PyTorch
     release. For more information, see
@@ -15,10 +16,10 @@ def set_seed(seed=None, seed_torch=True, seed_cudnn=False):
 
     Parameters
     ----------
-    seed: int
+    logger : logging.Logger object
+    seed : int
         Seed for random number generators.
-        Default: 2**32
-    seed_torch: bool
+    seed_torch : bool
         If we will set the seed for torch random number generators.
         Default: True
 
@@ -50,12 +51,12 @@ def set_seed(seed=None, seed_torch=True, seed_cudnn=False):
             # Ensure that the cuDNN convolution algorithm is deterministic
             torch.backends.cudnn.deterministic = True
 
-    print(f'Random seed {seed} has been set.')
+    logger.info(f'Random seed {seed} has been set.')
 
     return seed
 
 
-def seed_worker(worker_id):
+def seed_worker(worker_id, logger):
     """Set seed for Dataloader. DataLoader will reseed workers the "Randomness
     in multi-process data loading" algorithm. Use `worker_init_fn()` to
     preserve reproducibility. For more information, see
@@ -64,9 +65,10 @@ def seed_worker(worker_id):
     worker_seed = torch.initial_seed() % 2**32
     np.random.seed(worker_seed)
     random.seed(worker_seed)
+    msg = f"Dataloader worker {worker_id}'s seed {worker_seed} has been set."
+    logger.info(msg)
 
-
-def set_generator(seed=0):
+def get_generator(logger, seed=0):
     """Set seed for Dataloader generators. DataLoader will reseed workers the
     "Randomness in multi-process data loading" algorithm. Use `generator` to
     preserve reproducibility. For more information, see
@@ -74,36 +76,61 @@ def set_generator(seed=0):
 
     Parameters
     ----------
-    seed: int
+    logger : logging.Logger object
+    seed : int
         Seed for random number generators.
         Default: 0
 
     Returns
     -------
-    generator: torch.Generator
+    generator : torch.Generator
         Seeded torch generator
     """
     generator = torch.Generator()
     generator.manual_seed(seed)
 
+    msg = f"Dataloader generator with seed {worker_seed} has been created."
+    logger.info(msg)
+
     return generator
 
 
-def set_device():
+def get_device(target_devices, logger):
     """
-    Setup device (GPU or CPU).
+    Setup device: GPU if available, else CPU.
+
+    Parameters
+    ----------
+    target_devices : list of device IDs
+    logger : logging.Logger object
 
     Returns
     -------
-    device: str
-        The device to be used, either `cpu` or `gpu`
+    device: list[int]
+        A list of device pointers to be used
 
     """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    # Inform the user if torch  uses GPU or CPU.
-    if device != "cuda":
-        print("GPU is not enabled.")
-    else:
-        print("GPU is enabled.")
+    available_devices = list(range(torch.cuda.device_count()))
 
+    if not available_devices:
+        msg = ("There is no GPU available on this machine. "
+               "Training will be performed on CPU.")
+        logger.warning(msg)
+        device = torch.device('cpu')
+        return device
+
+    max_target_gpu = max(target_devices)
+    max_available_gpu = max(available_devices)
+
+    if max_target_gpu > max_available_gpu:
+        msg = (f"Configuration requestes GPU #{max_target_gpu} "
+               "but only {max_available_gpu} available. "
+               "Check the configuration and try agaion.")
+        logger.critical(msg)
+        raise Exception(msg)
+
+    msg = (f"Using devices {target_devices} of "
+           "available devices {available_devices}")
+    logger.info(msg)
+    device = torch.device(f'cuda:{target_devices[0]}')
     return device
