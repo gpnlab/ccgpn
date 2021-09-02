f2ed861ce35220396a74c55c2a03bc4c8f937cf6
eduardojdiniz <edd32@pitt.edu>
2021-09-02 15:54:13 -0400

    Add base_augmentation.py

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/base/__init__.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/base/__init__.py
@@ -1,3 +1,4 @@
+from .base_augmentation import AugmentationFactory
 from .base_dataloader import BaseDataLoader
 from .base_arch import BaseArch
 from .base_trainer import BaseTrainer, AverageMeter
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/base/base_augmentation.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/base/base_augmentation.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import abc
+from abc import ABC
+
+import torchvision.transforms as T
+
+
+class AugmentationFactory(ABC):
+    def build_transforms(self, train):
+        return self.build_train() if train else self.build_test()
+
+    @abc.abstractmethod
+    def build_train(self):
+        pass
+
+    @abc.abstractmethod
+    def build_test(self):
+        pass
